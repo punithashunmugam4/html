d9e78302152552f74e5dd410692ea829673d2a81
punithashunmugam4 <punithashunmugam4@gmail.com>
2022-12-30 12:59:03 +0530

    resolved

--- TipCalculator/index.js
+++ TipCalculator/index.js
@@ -7,28 +7,33 @@ const reset=document.getElementById("reset");
 const custom=document.querySelector(".custom");
 const customInput=document.querySelector(".customInput");
 
-
+let tipPercent=0;
 document.getElementById("one").addEventListener('click',()=>{
-   var tipPercent=document.getElementById("one").value;
+    reset.disabled=false;
+   tipPercent=document.getElementById("one").value;
     // console.log(tipPercent);
     // console.log(amount.value);
     // console.log(`$${amount.value*tipPercent}`); 
    calculate(tipPercent);
 })
 document.getElementById("two").addEventListener('click',()=>{
-   let tipPercent=document.getElementById("two").value;
+    reset.disabled=false;
+   tipPercent=document.getElementById("two").value;
    calculate(tipPercent);
 })
 document.getElementById("three").addEventListener('click',()=>{
-   let tipPercent=document.getElementById("three").value;
+    reset.disabled=false;
+   tipPercent=document.getElementById("three").value;
    calculate(tipPercent);
 })
 document.getElementById("four").addEventListener('click',()=>{
-   let tipPercent=document.getElementById("four").value;
+    reset.disabled=false;
+   tipPercent=document.getElementById("four").value;
    calculate(tipPercent);
 })
 document.getElementById("five").addEventListener('click',()=>{
-   let tipPercent=document.getElementById("five").value;
+    reset.disabled=false;
+   tipPercent=document.getElementById("five").value;
    calculate(tipPercent);
 })
 custom.addEventListener('click',()=>{
@@ -37,7 +42,7 @@ custom.addEventListener('click',()=>{
     customInput.style.display="block";
 })
 customInput.addEventListener('input',()=>{
-    let  tipPercent=(customInput.value/100).toFixed(2);
+    tipPercent=(customInput.value/100).toFixed(2);
   calculate(tipPercent);
 })
 
@@ -56,9 +61,11 @@ function calculate(tipPercent){
 
 amount.addEventListener('input',()=>{
     reset.disabled=false;
+    calculate(tipPercent);
 })
 people.addEventListener('input',()=>{
     reset.disabled=false;
+    calculate(tipPercent);
 })
 reset.addEventListener('click',()=>{
     tipPerPerson.innerHTML="$0.00";
@@ -67,4 +74,5 @@ reset.addEventListener('click',()=>{
     people.value="";
     custom.style.display="inline";
     customInput.style.display="none";
+    reset.disabled=true;
 })
